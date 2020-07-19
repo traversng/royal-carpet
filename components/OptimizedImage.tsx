36e8281c9167de24464e61901718c9e17ef3d1a9
Travis Ueki <travueki@gmail.com>
2020-07-19 18:16:29 -0500

    more image optimization

--- components/OptimizedImage.tsx
+++ components/OptimizedImage.tsx
@@ -11,8 +11,8 @@ export default ({
     <img
       className={`lazyload ${classes}`}
       alt={altText}
-      src={require(`../assets/images/${fileName}?webp`)}
-      data-srcset={`${require(`../assets/images/${fileName}`)} 1x, ${require(`../assets/images/${fileName}`)} 2x`}
+      src={require(`../assets/images/${fileName}?lqip`)}
+      srcSet={`${require(`../assets/images/${fileName}`)} 1x, ${require(`../assets/images/${fileName}`)} 2x`}
     />
   )
 }
