ac0ac08291811e05f7c4c2d3e821b124f0527bb2
Travis Ueki <travueki@gmail.com>
2020-07-19 13:15:06 -0500

    add chat

--- components/layouts/Default.tsx
+++ components/layouts/Default.tsx
@@ -1,12 +1,36 @@
+import React from 'react'
+
 import Navbar from '../Navbar'
 import Footer from '../Footer'
 
-export default function ({children}: {children: any}) {
-  return (
-    <div className="flex flex-col container mx-auto min-h-screen">
-      <Navbar />
-      <main className="md:mx-auto flex-col flex-grow md:flex md:justify-center">{children}</main>
-      <Footer />
-    </div>
-  )
+class Default extends React.Component {
+  componentDidMount() {
+    // Include the Crisp code here, without the <script></script> tags
+    window['$crisp'] = []
+    window['CRISP_WEBSITE_ID'] =
+      process.env.CRISP_ID || '2bba5423-0238-4656-a21a-b1b22117986a'
+
+    ;(function () {
+      var d = document
+      var s: any = d.createElement('script')
+
+      s.src = 'https://client.crisp.chat/l.js'
+      s.async = 1
+      d.getElementsByTagName('head')[0].appendChild(s)
+    })()
+  }
+
+  render() {
+    return (
+      <div className="flex flex-col container mx-auto min-h-screen">
+        <Navbar />
+        <main className="md:mx-auto flex-col flex-grow md:flex md:justify-center">
+          {this.props.children}
+        </main>
+        <Footer />
+      </div>
+    )
+  }
 }
+
+export default Default
