5fe2b3e68bec82069f2576cfdb77e73cd38d5a14
Travis Ueki <travueki@gmail.com>
2020-07-19 19:05:03 -0500

    add placeholder images

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,26 +1,27 @@
 import Link from 'next/link'
+import { FaFacebook, FaInstagram, FaYelp } from 'react-icons/fa'
 
 export default function () {
   return (
-    <footer className="flex mb-4 w-full py-2 justify-between md:mx-auto md:justify-center">
-      {/* <a
+    <footer className="flex w-full py-6 justify-between bg-purple-900 md:mx-auto md:justify-center">
+      <a
         href="https://github.com/traversng"
         className="cursor-pointer hover:text-blue-500 px-2"
       >
-        Github
+        <FaFacebook className="text-3xl text-white"/>
       </a>
       <a
         href="https://www.linkedin.com/in/travis-ueki/"
         className="cursor-pointer hover:text-blue-500 px-2"
       >
-        Linkedin
+        <FaInstagram className="text-3xl text-white"/>
       </a>
       <a
         href="https://twitter.com/traversng"
         className="cursor-pointer hover:text-blue-500 px-2"
       >
-        Twitter
-      </a> */}
+        <FaYelp className="text-3xl text-white"/>
+      </a>
     </footer>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -46,7 +46,7 @@ export default function Home() {
       </div>
 
       <div className="px-2">
-        <section className="mx-auto w-full px-4 md:py-6 flex flex-col items-center">
+        <section className="mx-auto md:w-2/3 my-4 px-4 md:py-6 flex flex-col items-center">
           <h3 className="text-3xl text-center font-extrabold text-purple-600 mb-2">
             Serving Austin
           </h3>
@@ -58,7 +58,7 @@ export default function Home() {
             reiciendis! Temporibus, natus. Veritatis.
           </p>
         </section>
-        <section className="mx-auto w-full px-4 md:py-6">
+        <section className="mx-auto w-full py-4 md:py-6 bg-gray-300">
           <div className="md:flex md:justify-center">
             <div className="md:w-4/5">
               <OptimizedCarousel content={carouselImages} />
@@ -66,15 +66,19 @@ export default function Home() {
           </div>
         </section>
         <section className="mx-auto w-full px-4 md:py-6">
-          <div className="md:flex my-8 md:py-20">
+          <div className="md:flex my-16 md:py-20">
             <OptimizedImage
-              fileName="baby-floor.jpg"
+              fileName="clean-carpet-feet.jpg"
               altText="baby-floor"
-              classes="md:w-1/3 rounded-md"
+              classes="w-full rounded-md"
             />
             <div className="md:p-16">
-              <h3 className="text-2xl text-purple-600 font-bold">Customer</h3>
-              <TiPhoneOutline className="text-3xl" />
+              <div className="flex">
+                <TiPhoneOutline className="hidden md:block text-3xl text-purple-800 m-2" />
+                <h3 className="text-2xl text-purple-600 font-bold">
+                  Carpet Cleaning
+                </h3>
+              </div>
               <p className="font-medium text-lg">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius,
                 dolores deserunt! Asperiores earum quidem dolore mollitia nam
@@ -83,14 +87,19 @@ export default function Home() {
               </p>
             </div>
           </div>
-          <div className="md:flex my-8 md:py-20 flex-row-reverse">
+          <div className="md:flex my-16 md:py-20 flex-row-reverse">
             <OptimizedImage
-              fileName="baby-floor.jpg"
+              fileName="tile-grout-child.jpg"
               altText="baby-floor"
-              classes="md:w-1/3 rounded-md"
+              classes="w-full rounded-md"
             />
             <div className="md:p-16">
-              <h3 className="text-2xl text-purple-600 font-bold">Header</h3>
+              <div className="flex">
+                <TiPhoneOutline className="hidden md:block text-3xl text-purple-800 m-2" />
+                <h3 className="text-2xl text-purple-600 font-bold">
+                  Tile & Grout
+                </h3>
+              </div>
               <p className="font-medium text-lg">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius,
                 dolores deserunt! Asperiores earum quidem dolore mollitia nam
@@ -99,14 +108,19 @@ export default function Home() {
               </p>
             </div>
           </div>
-          <div className="md:flex my-8 md:py-20">
+          <div className="md:flex my-16 md:py-20">
             <OptimizedImage
-              fileName="baby-floor.jpg"
+              fileName="upholstery-cleaning-dog-on-couch.jpg"
               altText="baby-floor"
-              classes="md:w-1/3 rounded-md"
+              classes="w-full rounded-md"
             />
             <div className="md:p-16">
-              <h3 className="text-2xl text-purple-600 font-bold">Header</h3>
+              <div className="flex">
+                <TiPhoneOutline className="hidden md:block text-3xl text-purple-800 m-2" />
+                <h3 className="text-2xl text-purple-600 font-bold">
+                  Upholstry
+                </h3>
+              </div>
               <p className="font-medium text-lg">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius,
                 dolores deserunt! Asperiores earum quidem dolore mollitia nam
