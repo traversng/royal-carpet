80d4d9d71517b75fcbc14276d0e7cee9bcdc8fda
Travis Ueki <travueki@gmail.com>
2020-07-19 21:25:29 -0500

    fix navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,45 +7,55 @@ import Close from '../components/svgs/Close'
 const links = [
   { text: 'About', url: '/about' },
   { text: 'Results', url: '/results' },
-  { text: 'Contact', url: '/contact' },
+  // { text: 'Contact', url: '/contact' },
+  { text: 'Social', url: '/social' },
 ]
 
 export default function () {
   const [hidden, setHidden] = useState(true)
 
   return (
-    <nav className="mx-auto flex px-2 py-2 justify-between w-full bg-purple-900 md:px-6 md:py-4">
-      <Link href="/">
-        <h2 className="text-2xl cursor-pointer font-extrabold flex-grow lg:flex-1">
-          <a className='text-white hover:text-purple-400'>Royal Carpet & Tile</a>
-        </h2>
-      </Link>
-      <div className='flex flex-col items-end'>
-        <div className="md:hidden">
-          <button onClick={() => setHidden(!hidden)}>
+    <>
+      <nav className="flex items-center justify-between flex-wrap bg-purple-900 p-6">
+        <div className="flex items-center flex-shrink-0 text-white mr-6">
+          <Link href="/">
+            <h3 className="cursor-pointer font-semibold text-3xl tracking-tight hover:text-purple-600">
+              Royal Carpet & Tile
+            </h3>
+          </Link>
+        </div>
+        <div className="block md:hidden">
+          {/* Hamburger */}
+          <button
+            className="rounded-md border border-white focus:outline-none"
+            onClick={() => setHidden(!hidden)}
+          >
             {hidden ? <Menu /> : <Close />}
           </button>
         </div>
-        <div className="flex justify-center md:justify-start">
-          <ul
-            className={`md:flex justify-between w-full md:w-1/2 ${
-              hidden ? 'hidden' : ''
-            }`}
+        <div className="w-full block flex-grow md:flex md:items-center md:w-auto">
+          <div
+            className={`text-sm md:flex-grow md:flex md:justify-end ${hidden ? 'hidden' : ''}`}
           >
-            {links.map((link) => {
-              return (
-                <Link href={link.url}>
-                  <li className="mx-1">
-                    <a className="text-xl font-bold text-white hover:text-purple-400" href="#">
+            <div>
+              {links.map((link) => {
+                return (
+                  <Link href={link.url}>
+                    <a className="mx-1 block mt-4 text-lg font-semibold md:inline-block md:mt-0 text-white hover:text-purple-600 mr-4">
                       {link.text}
                     </a>
-                  </li>
-                </Link>
-              )
-            })}
-          </ul>
+                  </Link>
+                )
+              })}
+              <Link href="contact">
+                <a className="inline-block text-lg px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-purple-600 hover:bg-white mt-4 md:mt-0">
+                  Contact
+                </a>
+              </Link>
+            </div>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </>
   )
 }
---
+++ pages/social.tsx
@@ -0,0 +1,23 @@
+import Default from '../components/layouts/Default'
+
+const Social = () => {
+  return (
+    <Default>
+      <div className="flex flex-col justify-center align-middle">
+        <h3 className="text-center text-3xl font-extrabold text-purple-600 mt-4 mb-2">
+          Social
+        </h3>
+        <hr className="border-t-4 self-center border-purple-600 w-1/12 mb-2 md:mb-4" />
+        <p className="font-medium text-lg text-center">We're Social Yall!</p>
+        <p className="font-medium text-lg text-center">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, dolores
+          deserunt! Asperiores earum quidem dolore mollitia nam magni culpa
+          modi, eligendi eaque exercitationem sunt ullam incidunt reiciendis!
+          Temporibus, natus. Veritatis.
+        </p>
+      </div>
+    </Default>
+  )
+}
+
+export default Social
