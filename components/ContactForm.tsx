f4c9aa0c6488dbf49e4d7be3e3628865b2c0c845
Travis Ueki <travueki@gmail.com>
2020-07-19 22:27:26 -0500

    update contact button

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -127,7 +127,7 @@ export default function () {
             )}
             <button
               type="submit"
-              className="bg-transparent hover:bg-white hover:text-black text-white font-semibold py-2 px-4 border border-white hover:border-transparent rounded"
+              className="btn"
             >
               {isSubmitting ? '...Sending' : 'Send'}
             </button>
