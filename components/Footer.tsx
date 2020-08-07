8ad6781b8bcfbd65019cadb08cf547aee2aadb4a
Travis Ueki <travueki@gmail.com>
2020-08-07 09:57:55 -0500

    footer image fix?

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,16 +5,16 @@ export default function () {
     <footer className="p-4 bg-gray-200 md:flex md:flex-col md:items-center md:justify-center md:px-6">
       <div className="p-4 w-full flex-col items-center justify-center md:flex md:flex-row">
         <div className="flex flex-col justify-between items-center md:flex-row">
-          <a className='flex justify-center' href="https://www.housecallpro.com/superpro/">
+          <a className='flex justify-center w-1/2 md:w-1/8' href="https://www.housecallpro.com/superpro/">
             <img
-              className='w-1/2 md:w-1/8'
+              className='w-full'
               src="http://images.ctfassets.net/990581qemznk/2tPWtrzJ1vUZvRiSNRB2LN/e21347485aaae488aaa41f6ecbccb155/Superpro_badge_Eagle_Level1.png"
               alt="Become A Housecall Pro SUPERPRO"
             />
           </a>
-          <a className='flex justify-center' href="https://www.housecallpro.com/superpro/">
+          <a className='flex justify-center w-1/2 md:w-1/8' href="https://www.housecallpro.com/superpro/">
             <img
-              className='w-1/2 md:w-1/8'
+              className='w-full'
               src="http://images.ctfassets.net/990581qemznk/6Pm2xJrLhwp8t7gDVmQq9T/bdaa5ff602f7dd36fee6646be00ec7e8/Superpro_badge_Plain_Level1.png"
               alt="Plain 2019 Superpro badge from Housecall Pro"
             />
