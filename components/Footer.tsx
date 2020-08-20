2321fa7dd32d5db726175914e4b7f9705b10db24
Travis Ueki <travueki@gmail.com>
2020-08-19 21:01:12 -0500

    add booking and gift certs

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -39,8 +39,12 @@ export default function () {
             >
               <BBB />
             </a>
-            <button className="btn w-full">Book Online Now</button>
-            <button className="btn w-full">Gift Certificates</button>
+            <a href="https://housecallpro.com/book/Royalty-Carpet--Tile-Cleaning/331682fdd7e241ecb6a8ac97c3680572">
+              <button className="btn w-full">Book Online Now</button>
+            </a>
+            <a href="https://giftup.app/place-order/c1164efb-f774-493d-8bcf-6da1b39ae972?platform=Squarespace">
+              <button className="btn w-full">Gift Certificates</button>
+            </a>
             <a
               href="https://rockincityrollergirls.org/"
               className="cursor-pointer hover:text-blue-500 px-2 w-full"
